99507234e4fd1e5b31eab52d19b366145593a959
DBENSAN <dftb97@gmail.com>
2023-02-03 17:16:29 -0800

    adds main prompt

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // Load dependancies
+const { Router } = require('express');
 const inquirer = require('inquirer')
 const mysql = require('mysql2');
 
@@ -12,6 +13,68 @@ const db = mysql.createConnection(
     console.log(`Connected to the employee_db database.`)
 );
 
-db.connect((err) => {
-    console.log(err)
-});
+
+async function init() {
+    let prompt = [{
+        name: "action",
+        type: "list",
+        message: "Select an action",
+        choices:
+            [
+                "View employees",
+                "View roles",
+                "View departments",
+                "Add department",
+                "Add role",
+                "Add employee",
+                "Edit employee",
+                "Remove employee",
+                "EXIT"
+            ]
+    }]
+
+    let data = await inquirer.prompt(prompt);
+    let { "action": answer } = data;
+
+    helper(answer)
+};
+
+function helper(value) {
+    if (value === "View employees") {
+        viewEmployees();
+     }
+    if (value === "View roles") {
+        viewRoles();
+     }
+    if (value === "View departments") {
+        viewDepartments();
+     }
+    if (value === "Add department") {
+        addDepartment();
+     }
+    if (value === "Add role") {
+        addRole()
+     }
+    if (value === "Edit employee") {
+        editEmployee()
+     }
+    if (value === "Remove employee") {
+        removeEmployee()
+     }
+    if (value === "EXIT") {
+        closeOut()
+     }
+};
+
+function viewEmployees(){
+    let query = `;`
+    db.query('', (err, res) =>{
+
+    })
+};
+
+
+
+
+init()
+
